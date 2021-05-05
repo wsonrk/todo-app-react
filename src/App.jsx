22f57dc12408d644ce78bc7fcf3a9ffdd4e5ed9c
Nor <71073186+wsonrk@users.noreply.github.com>
2021-05-05 14:52:21 +0900

    登録上限メッセージ追加

--- src/App.jsx
+++ src/App.jsx
@@ -45,7 +45,13 @@ export const App = () => {
         todoText={todoText}
         onChange={onChangeTodoText}
         onClick={onClickAdd}
+        disabled={incompleteTodos.length >= 5}
       />
+      {incompleteTodos.length >= 5 && (
+        <p style={{ color: "red" }}>
+          登録できるTODOは５個までです。消化してください。
+        </p>
+      )}
       <IncompleteTodos
         todos={incompleteTodos}
         onClickComplete={onClickComplete}
